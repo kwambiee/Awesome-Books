45c6941aea202a362d871c3398756395710569e0
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-19 11:55:01 +0300

    Function to add books dynamically

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ const getBooks = () => {
   const lib = localStorage.getItem('library');
   return lib ? JSON.parse(lib) : [];
 };
+const render = (books) => {
+  let booksList = '';
+  books.forEach((book, i) => {
+    const { title, author } = book;
+    booksList += `
+    <tr>
+      <th scope="row">${i + 1}</th>  
+      <td>${title}</td>
+      <td>${author}</td>
+      <td ><span onClick="removeBook(${i})" class="btn btn-danger btn-sm delete del py-2 px-3">Delete</span></td>
+    </tr>`;
+  });
+  return booksList;
+};
+const show = () => {
+  document.getElementById('table-body').innerHTML = render(getBooks());
+};
