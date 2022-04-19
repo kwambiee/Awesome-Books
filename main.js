c9cd0c7ae66e1b7b0f79593db47c497a35062bae
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-19 11:52:20 +0300

    Get books from Local storage

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ function Book() {
   this.title = document.querySelector('#title').value;
   this.author = document.querySelector('#author').value;
 }
+// get books from Local Storage
+const getBooks = () => {
+  const lib = localStorage.getItem('library');
+  return lib ? JSON.parse(lib) : [];
+};
