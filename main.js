c010934af13f61f0f609a073046d5b9e3a18a29c
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-19 21:54:33 +0300

    Fix Linter errors

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-const collection = document.querySelector(".table");
-
 class BookLists {
   constructor() {
-    this.bookList = JSON.parse(localStorage.getItem("library")) || [];
+    this.bookList = JSON.parse(localStorage.getItem('library')) || [];
   }
 
   // Display books and save in Local Storage
 
   showBooks() {
-    const bookTable = document.getElementById("table-body");
-    bookTable.innerHTML = "";
+    const bookTable = document.getElementById('table-body');
+    bookTable.innerHTML = '';
     this.bookList.forEach((book, index) => {
       bookTable.innerHTML += `
     <tr>
       <th scope="row">${index}</th>  
       <td>${book.title}</td>
       <td>${book.author}</td>
-      <td ><span id="${index}" onClick="remove(this.index)" class="btn btn-danger btn-sm delete del py-2 px-3">Delete</span></td>
+      <td ><span id="${index + 1}" onClick="remove(this.index)" class="btn btn-danger btn-sm delete del py-2 px-3">Delete</span></td>
     </tr>`;
     });
   }
@@ -29,37 +27,38 @@ class BookLists {
     };
 
     this.bookList.push(book);
-    localStorage.setItem("library", JSON.stringify(this.bookList));
+    localStorage.setItem('library', JSON.stringify(this.bookList));
   }
 
   // delete a book
   removeBook(bk) {
     this.bookList.splice(bk, 1);
-    localStorage.setItem("library", JSON.stringify(this.bookList));
+    localStorage.setItem('library', JSON.stringify(this.bookList));
     this.showBooks();
   }
 }
 
 const bookList = new BookLists();
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const addInput = document.querySelector("#button");
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addInput = document.querySelector('#button');
 
-console.log(bookList.showBooks());
+bookList.showBooks();
 
-function remove(index) {
+// eslint-disable-next-line no-unused-vars
+function remove(i) {
   //eslint-disable-line
-  bookList.removeBook(index);
+  bookList.removeBook(i);
 }
 
-addInput.addEventListener("click", (e) => {
+addInput.addEventListener('click', (e) => {
   e.preventDefault();
   if (titleInput.value && authorInput.value) {
     bookList.addBook(titleInput.value, authorInput.value);
     bookList.showBooks();
-    console.log((titleInput.value = ""));
-    console.log((authorInput.value = ""));
+    (titleInput.value = '');
+    (authorInput.value = '');
   }
 });
 // load books list when page loads
-window.onload = show();
+// window.onload = show();
