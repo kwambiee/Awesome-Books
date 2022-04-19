6b1c60ac8764b4b8d310c7b7fab67d81694a78be
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-19 21:40:34 +0300

    Use class methods to add and remove books

--- main.js
+++ main.js
@@ -1,73 +1,65 @@
-const bookForm = document.querySelector('#form');
-const collection = document.querySelector('.table');
+const collection = document.querySelector(".table");
 
-function Book() {
-  this.title = document.querySelector('#title').value;
-  this.author = document.querySelector('#author').value;
-}
-// get books from Local Storage
-const getBooks = () => {
-  const lib = localStorage.getItem('library');
-  return lib ? JSON.parse(lib) : [];
-};
-const render = (books) => {
-  let booksList = '';
-  books.forEach((book, i) => {
-    const { title, author } = book;
-    booksList += `
+class BookLists {
+  constructor() {
+    this.bookList = JSON.parse(localStorage.getItem("library")) || [];
+  }
+
+  // Display books and save in Local Storage
+
+  showBooks() {
+    const bookTable = document.getElementById("table-body");
+    bookTable.innerHTML = "";
+    this.bookList.forEach((book, index) => {
+      bookTable.innerHTML += `
     <tr>
-      <th scope="row">${i + 1}</th>  
-      <td>${title}</td>
-      <td>${author}</td>
-      <td ><span onClick="removeBook(${i})" class="btn btn-danger btn-sm delete del py-2 px-3">Delete</span></td>
+      <th scope="row">${index}</th>  
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td ><span id="${index}" onClick="remove(this.index)" class="btn btn-danger btn-sm delete del py-2 px-3">Delete</span></td>
     </tr>`;
-  });
-  return booksList;
-};
-const show = () => {
-  document.getElementById('table-body').innerHTML = render(getBooks());
-};
-const save = (book) => {
-  localStorage.setItem('library', JSON.stringify(book));
-  show();
-};
-// add a book to library array
-const addBook = (book) => {
-  const { title, author } = book; // object destructuring
-
-  const books = getBooks();
-  const dup = books.filter(
-    // eslint-disable-next-line comma-dangle
-    (item) => item.title === title && item.author === author
-  );
-  if (dup.length === 0 && title.length > 0) {
-    books.push({
-      title,
-      author,
     });
   }
-  return save(books);
-};
 
-// delete a book
-// eslint-disable-next-line no-unused-vars
-const removeBook = (bk) => {
-  const books = getBooks();
-  books.splice(bk, 1);
-  save(books);
-};
+  // add a book to library array
+  addBook(title, author) {
+    const book = {
+      title,
+      author,
+    };
 
-bookForm.addEventListener('submit', (e) => {
+    this.bookList.push(book);
+    localStorage.setItem("library", JSON.stringify(this.bookList));
+  }
+
+  // delete a book
+  removeBook(bk) {
+    this.bookList.splice(bk, 1);
+    localStorage.setItem("library", JSON.stringify(this.bookList));
+    this.showBooks();
+  }
+}
+
+const bookList = new BookLists();
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const addInput = document.querySelector("#button");
+
+console.log(bookList.showBooks());
+
+function remove(index) {
+  //eslint-disable-line
+  bookList.removeBook(index);
+}
+
+addInput.addEventListener("click", (e) => {
   e.preventDefault();
-  collection.style.display = 'block';
-});
-
-bookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const book = new Book();
-  addBook(book);
-  bookForm.reset();
-  show();
+  if (titleInput.value && authorInput.value) {
+    bookList.addBook(titleInput.value, authorInput.value);
+    bookList.showBooks();
+    console.log((titleInput.value = ""));
+    console.log((authorInput.value = ""));
+  }
 });
 // load books list when page loads
 window.onload = show();
